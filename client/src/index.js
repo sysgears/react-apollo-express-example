ae5615f5e5ef0d71685906eae35d7d406933a956
Sergey Gilevich <kentserg9453@gmail.com>
2018-12-13 17:05:45 +0200

    Add App component to the root component

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,6 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-
-const App = () => {
-  return <div>Hello React!</div>;
-};
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById("root"));
