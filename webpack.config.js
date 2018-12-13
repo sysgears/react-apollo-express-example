6d6c4b3559fe4ff45b5984d22ee2b8d7decf0851
Sergey Gilevich <kentserg9453@gmail.com>
2018-12-13 16:36:54 +0200

    Configurate htmlWebpackPlugin and add this one to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,13 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./client/src/index.html",
+  filename: "./index.html"
+});
+
 module.exports = {
+  entry: './client/src',
   module: {
     rules: [
       {
@@ -9,5 +18,6 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [htmlPlugin]
 };
