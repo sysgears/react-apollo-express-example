cd0e21a10988156832aaadc5864c76475f16f46f
Sergey Gilevich <kentserg9453@gmail.com>
2018-12-14 11:19:43 +0200

    Import reactstrap styles to root component

--- client/src/index.js
+++ client/src/index.js
@@ -3,4 +3,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(<App />, document.getElementById("root"));
