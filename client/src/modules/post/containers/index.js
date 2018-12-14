5ca3cbc03ee67f405ab0b54da3e8998e0cee7a49
Sergey Gilevich <kentserg9453@gmail.com>
2018-12-14 11:45:14 +0200

    Implement posts list rendering on the Posts page

--- client/src/modules/post/containers/index.js
+++ client/src/modules/post/containers/index.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react'
+import { Card, Button, CardTitle, CardText } from 'reactstrap';
 
 import { withPosts } from '../providers';
+
+import '../styles/styles.css';
 
 
 class Posts extends Component {
   render() {
+    const { posts, postsLoading } = this.props;
     return (
-      <div>
-        Posts
+      <div className="posts-container">
+        <h2 className="posts-title">Posts</h2>
+        {
+          !postsLoading && posts ? posts.map((post, index) => {
+            return (
+              <Card key={index} body className="text-center">
+                <CardTitle>{post.title}</CardTitle>
+                <CardText>{post.content}</CardText>
+              </Card>
+            );
+          }) : <div>Loading...</div>
+        }
+        
       </div>
     )
   }
