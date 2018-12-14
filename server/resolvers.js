cf90a35b828b8c8283b85c99e1bed96f664c8ffc
Sergey Gilevich <kentserg9453@gmail.com>
2018-12-14 16:01:13 +0200

    Add comments for resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,3 +1,4 @@
+// Dummy data
 const posts = [
   { title: 'Post Title1', content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' },
   { title: 'Post Title2', content: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." },
@@ -7,9 +8,13 @@ const posts = [
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
+    // Query which returns posts list
     posts: () => posts,
   },
   Mutation: {
+    /* Mutation which provides functionality for adding post to the posts list 
+    * and return post after successfully adding to list
+    */
     addPost: (_, post) => {
       posts.push(post);
       return post;
