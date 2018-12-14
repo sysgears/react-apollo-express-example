1ea423897927cd57dc8ba3a8b8bc887150643308
Sergey Gilevich <kentserg9453@gmail.com>
2018-12-14 18:21:44 +0200

    Add comments to the server file

--- server/server.js
+++ server/server.js
@@ -4,11 +4,15 @@ const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require('./graphqlSchema');
 const resolvers = require('./resolvers');
 
+// Create Apollo server
 const server = new ApolloServer({ typeDefs, resolvers });
 
+// Create Express app
 const app = express();
+
 server.applyMiddleware({ app });
 
+// Listen server
 app.listen({ port: 3000 }, () =>
   console.log(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`)
 );
