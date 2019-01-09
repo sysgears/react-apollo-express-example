75d7ebf480a60ad8543fad6e7fe355be7eaa1e81
Sergey Gilevich <kentserg9453@gmail.com>
2019-01-09 12:14:40 +0200

    Change resolver functionality for getting posts list and adding post to list

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,18 +1,24 @@
-const Post = require('./models/post.model');
+// Dummy data
+const posts = [
+  { title: 'Post Title1', content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.' },
+  { title: 'Post Title2', content: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." },
+  { title: 'Post Title3', content: 'Contrary to popular belief, Lorem Ipsum is not simply random text. ' }
+];
+
 
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
     // Query which returns posts list
-    posts: () => Post.find({})
+    posts: () => posts
   },
   Mutation: {
     /* Mutation which provides functionality for adding post to the posts list
     * and return post after successfully adding to list
     */
     addPost: (_, post) => {
-      const newPost = new Post({ title: post.title, content: post.content });
-      return newPost.save();
+      posts.push(post);
+      return post
     }
   }
 };
