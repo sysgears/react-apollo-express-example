ded0c90fc1046f94e0b7239b965be5bf3aaec3f0
Sergey Gilevich <kentserg9453@gmail.com>
2019-01-09 12:16:30 +0200

    Update functionality for submiting post from

--- client/src/modules/post/containers/PostForm.js
+++ client/src/modules/post/containers/PostForm.js
@@ -1,60 +1,41 @@
 import React, { Component } from 'react';
-import { Mutation } from 'react-apollo';
-import { gql } from "apollo-boost";
-import { GET_POSTS } from "../providers/PostsList";
+
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import { withAddPost } from '../providers';
 
-const ADD_POST = gql`
-  mutation($title: String!, $content: String!) {
-    addPost(title: $title, content: $content) {
-      title
-      content
-    }
-  }
-`;
-
-export default class PostForm extends Component {
+class PostForm extends Component {
   constructor(props) {
     super(props);
     this.submitForm = this.submitForm.bind(this);
   }
 
-  submitForm(event, addPost) {
+  submitForm(event) {
     event.preventDefault();
 
-    addPost({
-      variables: {
-        title: event.target.title.value,
-        content: event.target.content.value,
-      },
-      refetchQueries: [
-        { query: GET_POSTS }
-      ]
+    this.props.addPost({
+      title: event.target.title.value,
+      content: event.target.content.value
     });
   }
 
   render() {
     return (
-      <Mutation mutation={ADD_POST}>
-        {(addPost) => {
-          return (
-            <div className="post-form">
-              <h2>Create new post</h2>
-              <Form onSubmit={(event) => this.submitForm(event, addPost)}>
-                <FormGroup>
-                  <Label for="postTitle">Post Title</Label>
-                  <Input type="text" name="title" id="postTitle" placeholder="Add a title" />
-                </FormGroup>
-                <FormGroup>
-                  <Label for="postContent">Post Content</Label>
-                  <Input type="textarea" name="content" id="postContent" placeholder="Describe your post" />
-                </FormGroup>
-                <Button className="submit-button">Submit new post</Button>
-              </Form>
-            </div>
-          )
-        }}
-      </Mutation>
+      <div className="post-form">
+        <h2>Create new post</h2>
+        <Form onSubmit={(event) => this.submitForm(event)}>
+          <FormGroup>
+            <Label for="postTitle">Post Title</Label>
+            <Input type="text" name="title" id="postTitle" placeholder="Add a title" />
+          </FormGroup>
+          <FormGroup>
+            <Label for="postContent">Post Content</Label>
+            <Input type="textarea" name="content" id="postContent" placeholder="Describe your post" />
+          </FormGroup>
+          <Button className="submit-button">Submit new post</Button>
+        </Form>
+      </div>
     )
   }
 }
+
+export default withAddPost(PostForm);
